e88ddbb16113dacf7c68fb99e8983d97299ca1ef
NikitaGryaznov <nikitagryaznov1@gmail.com>
2020-02-18 13:01:59 +0300

    transit pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,7 +6,6 @@ from .login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        #login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
