876a4725d67ec6acdb3e5a994879f950ec3882a7
NikitaGryaznov <nikitagryaznov1@gmail.com>
2020-02-18 17:47:47 +0300

    add files: product_page.py; test_product_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -1,15 +1,18 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
-class LoginPageLocators():
+class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
-    #ENTHER_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
-    #ENTHER_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
-    #REG_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
-    #REG_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
-    #REG_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+
+
+class ProductPageLocators:
+    ADD_TO_CARD = (By.XPATH, "//button[text()='Добавить в корзину']")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner ")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
